57fb0660f4cefb2644965d79f11f5880f53288ca
Jean-Francois Pessiot <jf.pessiot@gmail.com>
2018-02-13 01:03:47 +0900

    Add get_cae, rename get_semisup_cae

--- models.py
+++ models.py
@@ -20,6 +20,7 @@ def get_convnet(img_width, img_height, filter_counts, filter_size, downsampling,
         x = MaxPooling2D((d, d), padding='same')(x)
 
     encoded = x
+    encoder = Model(input_img, encoded)
 
     z = Flatten()(encoded)
     for sup_layer in sup_layers:
@@ -30,11 +31,32 @@ def get_convnet(img_width, img_height, filter_counts, filter_size, downsampling,
     model = Model(input_img, z)
     model.compile(optimizer='rmsprop', loss='binary_crossentropy',
                   metrics=['accuracy'])
-    return model
+    return {'model': model, 'encoder': encoder}
 
 
 def get_cae(img_width, img_height, filter_counts, filter_size, downsampling,
-            sup_weight, transpose, sup_layers):
+            transpose, encoder, freeze_encoder):
+    input_img = Input(shape=(img_width, img_height, 1))
+    if freeze_encoder:
+        for layer in encoder.layers:
+            layer.trainable = False
+    x = encoder(input_img)
+
+    for f, d in zip(filter_counts[::-1], downsampling[::-1]):
+        if transpose:
+            x = Conv2DTranspose(f, filter_size, strides=(d, d),
+                                activation='relu', padding='same')(x)
+        else:
+            x = Conv2D(f, filter_size, activation='relu', padding='same')(x)
+            x = UpSampling2D((d, d))(x)
+    decoded = Conv2D(1, filter_size, activation='sigmoid', padding='same')(x)
+    autoencoder = Model(input_img, decoded)
+    autoencoder.compile(optimizer='rmsprop', loss='binary_crossentropy')
+    return {'model': autoencoder}
+
+
+def get_semisup_cae(img_width, img_height, filter_counts, filter_size,
+                    downsampling, sup_weight, transpose, sup_layers):
     input_img = Input(shape=(img_width, img_height, 1))
     x = input_img
     for f, d in zip(filter_counts, downsampling):
